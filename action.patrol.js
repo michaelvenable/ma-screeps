6df21d4ec18053acf3c587f8c7fc96a6b13e87f9
Michael Venable <mvenable@gmx.com>
2017-06-11 20:14:08 -0500

    Do not attack any creeps belonging to Chris Hobak

--- action.patrol.js
+++ action.patrol.js
@@ -1,29 +1,29 @@
 function run(creep) {
     let intruders = creep.room.find(FIND_HOSTILE_CREEPS);
-    
+
     // If there are intruders, attack them.
-    /*
     if (intruders.length > 0) {
         let intruder = intruders[0];
-        let result = creep.attack(intruder)
-        if (result === OK) {
-            console.log(`${creep} is attacking ${intruder}`);
-        } else if (result === ERR_NOT_IN_RANGE) {
-            console.log(`${creep} is moving in to attack ${intruder}`);
-            creep.moveTo(intruder);
+        if (intruder.owner.username !== 'HOBAKK') {
+          let result = creep.attack(intruder)
+          if (result === OK) {
+              console.log(`${creep} is attacking ${intruder}`);
+          } else if (result === ERR_NOT_IN_RANGE) {
+              console.log(`${creep} is moving in to attack ${intruder}`);
+              creep.moveTo(intruder);
+          }
+          return;
         }
-        return;
     }
-    */
 
     // Otherwise, patrol around the room every 3 ticks. But don't go outside the room.
     if (creep.memory.lastPatrolTime === undefined || Game.time >= creep.memory.lastPatrolTime + 4) {
         let directions = [TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT, TOP_LEFT];
         let index = Math.floor(Math.random() * (directions.length - 1));
         let direction = directions[index];
-        
+
         creep.move(direction);
-        
+
         creep.memory.lastPatrolTime = Game.time;
     }
 }
