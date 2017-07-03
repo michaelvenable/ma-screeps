6d01777ad46fead33c9a81c9be15b32080e97a5b
Michael Venable <mvenable@gmx.com>
2017-07-02 23:39:19 -0500

    Place construction sites

--- ai.architect.js
+++ ai.architect.js
@@ -29,19 +29,39 @@ function run() {
         let room = Game.rooms[name];
 
         // Skip if we've already created a build list for this room.
-        if (Memory.buildLists !== undefined && Memory.buildLists[name] !== undefined) {
-           continue;
+        if (Memory.buildLists === undefined || Memory.buildLists[name] === undefined) {
+            let map = mapping.structureMap.createFromRoom(room);
+            let buildList = [];
+
+            employedStrategies.forEach(strategy => strategy.run(room, map, buildList));
+
+            mapping.structureMap.print(map);
+
+            Memory.buildLists = Memory.buildList || {};
+            Memory.buildLists[name] = buildList;
+        } else {
+            if (Memory.lastBuildTime === undefined) {
+                Memory.lastBuildTime = Game.time;
+            }
+
+            if (Game.time >= (Memory.lastBuildTime + 20)) {
+                console.log("Placing construction sites. Build list has items: ", Memory.buildLists[name].length);
+
+                Memory.lastBuildTime = Game.time;
+
+                let nextBuildList = [];
+
+                while (Memory.buildLists[name].length > 0) {
+                    let action = Memory.buildLists[name].shift();
+                    let result = room.createConstructionSite(action.pos.x, action.pos.y, action.type);
+                    if (result !== OK) {
+                        nextBuildList.push(action);
+                    }
+                }
+
+                Memory.buildLists[name] = nextBuildList;
+            }
         }
-
-        let map = mapping.structureMap.createFromRoom(room);
-        let buildList = [];
-
-        employedStrategies.forEach(strategy => strategy.run(room, map, buildList));
-
-        mapping.structureMap.print(map);
-
-        Memory.buildLists = Memory.buildList || {};
-        Memory.buildLists[name] = buildList;
     }
 }
 
