5185753b5f91e697ab710a476b31c3d2fc25b2e0
Michael Venable <mvenable@gmx.com>
2017-07-03 00:21:04 -0500

    Build structure map in sections

--- ai.architect.js
+++ ai.architect.js
@@ -14,52 +14,69 @@ function run() {
     // }
 
     // Memory.lastBuildTime = Game.time;
+    //
+
+    Memory.architect = Memory.architect || {};
+
+    if (Memory.architect.startPlanTimestamp === undefined) {
+        Memory.architect.startPlanTimestamp = Game.time;
+    }
 
     let employedStrategies = [
-        strategies.buildLinksNearEnergySources,
-        strategies.buildLinksNearSpawns,
-        strategies.buildTowersNearSpawns,
-        strategies.buildTowersNearControllers,
-        strategies.buildExtensions,
-        strategies.buildRoadsFromSpawnToEnergy,
-        strategies.buildRoadsFromEnergyToController
+        { at: 1, fn: strategies.buildLinksNearEnergySources },
+        { at: 5, fn: strategies.buildLinksNearSpawns },
+        { at: 10, fn: strategies.buildTowersNearSpawns },
+        { at: 15, fn: strategies.buildTowersNearControllers },
+        { at: 20, fn: strategies.buildExtensions },
+        { at: 40, fn: strategies.buildRoadsFromSpawnToEnergy },
+        { at: 80, fn: strategies.buildRoadsFromEnergyToController }
     ];
 
     for (let name in Game.rooms) {
         let room = Game.rooms[name];
 
         // Skip if we've already created a build list for this room.
-        if (Memory.buildLists === undefined || Memory.buildLists[name] === undefined) {
-            let map = mapping.structureMap.createFromRoom(room);
-            let buildList = [];
+        // if (Memory.buildLists === undefined || Memory.buildLists[name] === undefined) {
+        if (Game.time < Memory.architect.startPlanTimestamp + 100) {
+            Memory.architect.maps = Memory.architect.maps || {};
+            if (Memory.architect.maps[name] === undefined) {
+                Memory.architect.maps[name] = mapping.structureMap.createFromRoom(room);
+            }
+            let map = Memory.architect.maps[name];
 
-            employedStrategies.forEach(strategy => strategy.run(room, map, buildList));
+            Memory.architect.buildLists = Memory.architect.buildLists || {};
+            let buildList = Memory.architect.buildLists[name] || [];
 
-            mapping.structureMap.print(map);
+            employedStrategies
+                .filter(strategy => Game.time === Memory.architect.startPlanTimestamp + strategy.at)
+                .forEach(strategy => {
+                    strategy.fn.run(room, map, buildList)
+                    mapping.structureMap.print(map);
+                });
 
-            Memory.buildLists = Memory.buildList || {};
-            Memory.buildLists[name] = buildList;
+            Memory.architect.buildLists[name] = buildList;
         } else {
             if (Memory.lastBuildTime === undefined) {
                 Memory.lastBuildTime = Game.time;
             }
 
-            if (Game.time >= (Memory.lastBuildTime + 20)) {
-                console.log("Placing construction sites. Build list has items: ", Memory.buildLists[name].length);
+            if (Game.time >= (Memory.lastBuildTime + 200)) {
+                console.log("Placing construction sites. Build list has items: ", Memory.architect.buildLists[name].length);
 
                 Memory.lastBuildTime = Game.time;
 
                 let nextBuildList = [];
 
-                while (Memory.buildLists[name].length > 0) {
-                    let action = Memory.buildLists[name].shift();
+                while (Memory.architect.buildLists[name].length > 0) {
+                    let action = Memory.architect.buildLists[name].shift();
                     let result = room.createConstructionSite(action.pos.x, action.pos.y, action.type);
                     if (result !== OK) {
                         nextBuildList.push(action);
                     }
                 }
 
-                Memory.buildLists[name] = nextBuildList;
+                Memory.architect.buildLists[name] = nextBuildList;
+                Memory.lastBuildTime = Game.time;
             }
         }
     }
