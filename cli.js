587ad648e6f8b77cb93960c5acde507a3fd1b7af
Michael Venable <mvenable@gmx.com>
2017-07-05 20:45:25 -0500

    Add CLI help command

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 module.exports = {
     bulldozeRoom: require('cli.bulldoze-room'),
+    help: require('cli.help'),
     showJobs: require('cli.show-jobs'),
     scheduleTask: require('cli.schedule-task')
 }
