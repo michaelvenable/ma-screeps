2a5a45553e676f105377c5b1636948abb0f41274
Michael Venable <mvenable@gmx.com>
2017-07-23 00:08:31 -0500

    Add command to spawn creeps

--- spawns.commands.js
+++ spawns.commands.js
@@ -1,4 +1,5 @@
 module.exports = {
     nextPlan: require('spawns.commands.next-plan'),
-    plan: require('spawns.commands.plan')
+    plan: require('spawns.commands.plan'),
+    spawn: require('spawns.commands.spawn')
 };
---
+++ spawns.commands.spawn.js
@@ -0,0 +1,38 @@
+let roles = require('constants').roles;
+
+function run() {
+    let spawns = [];
+
+    for (let name in Game.spawns) {
+        spawns.push(Game.spawns[name]);
+    }
+
+    spawns.forEach(spawn => {
+        if (spawn.spawning) {
+            return;
+        }
+
+        let numPeasants = spawn.room.find(FIND_MY_CREEPS).filter(creep => creep.memory.role === 'peasant').length;
+
+        let plan = Memory.spawns.currentPlan;
+
+        if (numPeasants >= plan.count) {
+            return;
+        }
+
+        if (spawn.room.energyCapacityAvailable >= getCost(plan.parts)) {
+            let memory = {
+                role: roles.peasant,
+            };
+
+            let result = spawn.createCreep(plan.parts, undefined, memory);
+            console.log(`Architect: Spawning a peasant: ${JSON.stringify(plan.parts)}: ${result}`);
+        }
+    });
+}
+
+function getCost(parts) {
+    return parts.reduce((sum, part) => sum + BODYPART_COST[part], 0);
+}
+
+module.exports = run;
